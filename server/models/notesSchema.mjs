fac19fcd3db179ba9e9842e4fc84c1de22515bc4
happybird22 <dungeonsnotdating@gmail.com>
2025-06-02 12:43:52 -0600

    added session note schema

---
+++ server/models/notesSchema.mjs
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema(
+    {
+        campaignTitle: {
+            type: String,
+            required: true,
+        },
+        sessionDate: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        },        
+        notes: {
+            type: String,
+            required: true,
+        },
+        notableNPCs: {
+            type: String,
+        },
+        notablePlaces: {
+            type: String,
+        },
+
+        memorableMoments: {
+            type: String,
+        },
+
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        partyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Party', required: true },
+
+        visibility: {
+            type: String,
+            enum: ['private', 'all', 'one'],
+            default: 'private',
+        },
+        sharedWith: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],        
+    },
+    { timestamps: true }
+);
+
+const SessionNote = mongoose.model('SessionNote', noteSchema);
+export default SessionNote;
