07be8519c32630888ccccb2187d17150fa38453e
happybird22 <dungeonsnotdating@gmail.com>
2025-06-02 09:53:44 -0600

    added password hashing

--- server/models/userSchema.mjs
+++ server/models/userSchema.mjs
@@ -34,3 +34,21 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
