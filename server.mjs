63a19f59cb9d8dc0b0c93239d7f34aee5c80e278
happybird22 <dungeonsnotdating@gmail.com>
2025-06-05 12:39:18 -0600

    installed cors and added it to middleware

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 // Imports
+import cors from 'cors';
 import express from 'express';
 import dotenv from "dotenv";
 import connectDB from './server/db/conn.mjs';
@@ -14,6 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors({
+    origin: 'http;//localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
 
 // Routes
